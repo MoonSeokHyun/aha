568e8a5c40b80090d9f15f0d0fa7e10ce97908de
Seoki <mls0000@naver.com>
2023-09-19 16:40:47 +0900

    추가

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,4 +47,66 @@
                 </div>
             </div>
         </section>
+        <section>
+            <div style="text-align: center; margin-bottom: 20px;">
+                <h2>내 주변 교육기관 알아보기</h2>
+            </div>
+            <div id="map" style="width:100%;height:400px;"></div>
+        </section>
+        
+
+        <div id="result"></div>
+
+
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                var mapOptions = {
+                    center: new naver.maps.LatLng(37.566826, 126.9786567),
+                    zoom: 10
+                };
+
+                var map = new naver.maps.Map('map', mapOptions);
+                var geocoder = new naver.maps.Service.Geocoder();
+
+                geocoder.coordToAddress(126.9786567, 37.566826, function(status, response) {
+                    if (status === naver.maps.Service.Status.ERROR) {
+                        return alert('Something went wrong!');
+                    }
+
+                    var item = response.v2.address;
+                    var address = item.jibunAddress;
+                    document.getElementById('result').innerHTML = '주소: ' + address;
+                });
+            });
+        </script>   
+
+        <script>
+            var mapOptions = {
+                center: new naver.maps.LatLng(37.5665, 126.9780), // 초기 위치: 서울시청
+                zoom: 11
+            };
+    
+            var map = new naver.maps.Map('map', mapOptions);
+    
+            // HTML5의 geolocation으로 사용할 수 있는지 확인합니다.
+            if ("geolocation" in navigator) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var lat = position.coords.latitude,
+                        lon = position.coords.longitude;
+    
+                    var location = new naver.maps.LatLng(lat, lon);
+    
+                    map.setCenter(location); // 얻은 좌표를 지도의 중심으로 설정합니다.
+                    map.setZoom(15); // 지도의 줌 레벨을 변경합니다.
+    
+                    // 지도에 마커를 표시합니다.
+                    new naver.maps.Marker({
+                        map: map,
+                        position: location
+                    });
+                });
+            } else {
+                alert("이 브라우저에서는 Geolocation이 지원되지 않습니다.")
+            }
+        </script>
         @endsection
