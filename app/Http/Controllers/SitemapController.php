fc6dbf46713ffd04ab0cb24f845bf7ab2fe20e6a
Seoki <mls0000@naver.com>
2023-09-26 00:41:48 +0900

    사이트맵 초기화

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,36 +2,79 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Queue;
-use App\Jobs\GenerateSitemap;
 use App\Models\ChildcareCenter;
 use App\Models\Kindergarten;
 use App\Models\AcademyInfo;
-use App\Models\PublicServiceInfo;
-
+use Illuminate\Http\Request;
 
 class SitemapController extends Controller
 {
     public function index()
     {
-        $models = [
-            ChildcareCenter::class,
-            Kindergarten::class,
-            AcademyInfo::class,
-            PublicServiceInfo::class
-        ];
-    
-        foreach ($models as $model) {
-            $totalDataCount = $model::count();
-            $chunkSize = 2000;
-    
-            for ($i = 0; $i < $totalDataCount; $i += $chunkSize) {
-                $fileCount = ($i / $chunkSize) + 1;
-                Queue::push(new GenerateSitemap($model, $fileCount, $i, $chunkSize));
-            }
-        }
-    
-        return response()->json(['message' => 'Sitemap generation started.']);
+        $sitemapIndex = [];
+        $fileCount = 1;
+        $chunkSize = 20000;
+
+        // 어린이집 정보 추가
+        $this->processData(ChildcareCenter::query(), 'childcare', $chunkSize, $sitemapIndex, $fileCount);
+
+        // 유치원 정보 추가
+        $this->processData(Kindergarten::query(), 'kindergartens', $chunkSize, $sitemapIndex, $fileCount);
+
+        // 학원 정보 추가
+        $this->processData(AcademyInfo::query(), 'academy_info', $chunkSize, $sitemapIndex, $fileCount);
+
+        // 중복 제거
+        $sitemapIndex = array_unique($sitemapIndex);
+
+        // Write sitemap index
+        $sitemapIndexXml = view('sitemap_index', ['sitemaps' => $sitemapIndex])->render();
+        $filePath = public_path('sitemap_index.xml');
+        file_put_contents($filePath, $sitemapIndexXml);
+
+        return response()->file($filePath);
     }
-    
+
+    private function processData($query, $pathSegment, $chunkSize, &$sitemapIndex, &$fileCount)
+    {
+        $count = 0;
+        $urls = [];
+
+        $query->chunk($chunkSize, function ($items) use (&$count, &$urls, &$fileCount, &$sitemapIndex, $pathSegment) {
+            foreach ($items as $item) {
+                $count++;
+                $lastmod = $item->updated_at ? $item->updated_at->toAtomString() : now()->toAtomString();
+                $urls[] = [
+                    'loc' => 'https://rangkingedu.shop/' . $pathSegment . '/' . $item->id,
+                    'lastmod' => $lastmod,
+                    'changefreq' => 'weekly',
+                    'priority' => '0.8',
+                ];
+
+                if ($count >= 50000) {
+                    $this->writeSitemap($urls, $fileCount);
+                    $sitemapIndex[] = url("sitemap{$fileCount}.xml.gz");
+                    $urls = [];
+                    $count = 0;
+                    $fileCount++;
+                }
+            }
+        });
+
+        // If there are remaining URLs that haven't been written, write them
+        if ($count > 0) {
+            $this->writeSitemap($urls, $fileCount);
+            $sitemapIndex[] = url("sitemap{$fileCount}.xml.gz");
+            $fileCount++;
+        }
+    }
+
+    private function writeSitemap($urls, $fileCount)
+    {
+        $sitemap = view('sitemap', ['urls' => $urls])->render();
+        $compressed = gzencode($sitemap, 9);
+        $filePath = public_path("sitemap{$fileCount}.xml.gz");
+
+        file_put_contents($filePath, $compressed);
+    }
 }
--- app/Jobs/GenerateSitemap.php
+++ app/Jobs/GenerateSitemap.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
 
 class GenerateSitemap implements ShouldQueue
 {
@@ -27,37 +28,29 @@ class GenerateSitemap implements ShouldQueue
 
     public function handle()
     {
-        $sitemapIndex = [];
         $query = $this->model::query()->offset($this->offset)->limit($this->limit);
         $pathSegment = strtolower(class_basename($this->model));
+
+        $sitemapIndex = Cache::get('sitemapIndex', []);
         $this->processData($query, $pathSegment, $sitemapIndex, $this->fileCount);
 
-        $sitemapIndexXml = view('sitemap_index', ['sitemaps' => $sitemapIndex])->render();
-        $filePath = public_path('sitemap_index.xml');
-        file_put_contents($filePath, $sitemapIndexXml);
+        Cache::forever('sitemapIndex', $sitemapIndex);
     }
 
     private function processData($query, $pathSegment, &$sitemapIndex, $fileCount)
     {
         $urls = [];
-        $query->each(function ($item) use (&$urls, $pathSegment, $fileCount, &$sitemapIndex) {
-            $filePath = public_path("sitemap{$fileCount}.xml.gz");
-
-            if (file_exists($filePath)) {
-                return;
-            }
-
-            $lastmod = $item->updated_at ? $item->updated_at->toAtomString() : now()->toAtomString();
+        $query->each(function ($item) use (&$urls, $pathSegment) {
             $urls[] = [
                 'loc' => 'https://rangkingedu.shop/' . $pathSegment . '/' . $item->id,
-                'lastmod' => $lastmod,
+                'lastmod' => $item->updated_at ? $item->updated_at->toAtomString() : now()->toAtomString(),
                 'changefreq' => 'weekly',
                 'priority' => '0.8',
             ];
         });
 
         $this->writeSitemap($urls, $fileCount);
-        $sitemapIndex[] = url("sitemap{$fileCount}.xml.gz");
+        $sitemapIndex[] = 'https://rangkingedu.shop/sitemap' . $fileCount . '.xml.gz';
     }
 
     private function writeSitemap($urls, $fileCount)
